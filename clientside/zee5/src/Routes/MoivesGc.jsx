1e1fc0bd405f203508f3c919e0d4c362404dd56a
Gowtham-Chokkalingam <gowthamtceian@gmail.com>
2022-11-12 20:47:04 +0530

    Loding and Routing are done

--- clientside/zee5/src/Routes/MoivesGc.jsx
+++ clientside/zee5/src/Routes/MoivesGc.jsx
@@ -27,7 +27,7 @@ const MoivesGc = () => {
     const timer = setTimeout(() => {
       console.log("setTimeout:", i++);
       setLoading(false);
-    }, 25000);
+    }, 2000);
     return () => clearTimeout(timer);
   }, []);
   return loading ? (
